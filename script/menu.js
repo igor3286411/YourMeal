802b1bcf63063faf47bd6337afbf04b2fe80264a
igor3286411 <139371965+igor3286411@users.noreply.github.com>
2023-11-07 18:14:27 +0100

    Add files via upload

---
+++ script/menu.js
@@ -0,0 +1,82 @@
+const hamburgers = [
+    {
+        name: 'Мясная бомба',
+        price: 689,
+        weight: 520,
+        src: './images/menu/burger1.jpg'
+    },
+    {
+        name: 'Супер сырный',
+        price: 550,
+        weight: 512,
+        src: './images/menu/burger2.jpg'
+    },
+    {
+        name: 'Сытный',
+        price: 689,
+        weight: 520,
+        src: './images/menu/burger3.jpg'
+    },
+    {
+        name: 'Тяжелый удар',
+        price: 480,
+        weight: 470,
+        src: './images/menu/burger4.jpg'
+    },
+    {
+        name: 'Вечная классика',
+        price: 450,
+        weight: 450,
+        src: './images/menu/burger5.jpg'
+    },
+    {
+        name: 'Итальянский',
+        price: 560,
+        weight: 510,
+        src: './images/menu/burger6.jpg'
+    }
+]
+
+const snacks = [
+    {
+        name: 'Начос',
+        price: 250,
+        weight: 220,
+        src: './images/menu/snack1.jpg'
+    },
+    {
+        name: 'Картошка фри',
+        price: 245,
+        weight: 180,
+        src: './images/menu/snack2.jpg'
+    },
+    {
+        name: 'Луковые кольца',
+        price: 230,
+        weight: 160,
+        src: './images/menu/snack3.jpg'
+    }
+]
+
+const hotDogs = [
+    {
+        name: 'Домашний хот-дог',
+        price: 290,
+        weight: 250,
+        src: './images/menu/hotDog1.jpg'
+    },
+    {
+        name: 'Жгучий хот-дог',
+        price: 239,
+        weight: 245,
+        src: './images/menu/hotDog2.jpg'
+    },
+    {
+        name: 'Классический хот-дог',
+        price: 220,
+        weight: 215,
+        src: './images/menu/hotDog3.jpg'
+    }
+]
+
+export {hamburgers, snacks, hotDogs};
