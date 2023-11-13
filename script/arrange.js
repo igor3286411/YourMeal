56daee4c16f9b3f72096604c692be945a8026f87
igor3286411 <139371965+igor3286411@users.noreply.github.com>
2023-11-13 16:52:02 +0100

    Add files via upload

--- script/arrange.js
+++ script/arrange.js
@@ -2,8 +2,14 @@ const deliverySection = document.querySelector('.delivery-section')
 const deliveryForm = document.querySelector('.delivery__form')
 const formClose = document.querySelector('.delivery__form-close')
 const formDelivery = document.querySelector('.delivery__form-pickup.deliverytis')
+const basketOrderMobile = document.querySelector('.basket__order')
+const basketMobile = document.querySelector('.basket__top')
 
 buttonBuyActive.addEventListener('click', () => deliverySection.classList.add('open'))
+
+basketMobile.addEventListener('click', () => {
+    basketOrderMobile.style.display = 'block'
+})
 
 deliveryForm.addEventListener('click', (e) =>{
     e.preventDefault()
